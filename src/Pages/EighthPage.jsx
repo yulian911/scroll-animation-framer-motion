4fff83ba35655dbf88a1e5a1c7435dc5104e82f9
Yulian Melnychuk <julian95@o2.pl>
2023-02-15 15:18:36 +0100

    update

--- src/Pages/EighthPage.jsx
+++ src/Pages/EighthPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createElement, Children, useRef } from 'react';
 import { AiFillFacebook, AiFillGithub, AiFillInstagram } from 'react-icons/ai';
 import { one } from '../assets';
 import './eightPage.css';
@@ -9,13 +9,29 @@ const data = [
   { bg: '#0a66c2', text: 'instagram', icon: <AiFillInstagram size={30} color="#fff" /> },
 ];
 
+const spans = [];
 const EighthPage = () => {
   const [active, setActive] = useState(false);
   const [hh, setHH] = useState(new Date().getHours());
   const [ss, setSS] = useState(new Date().getSeconds());
   const [mm, setMM] = useState(new Date().getMinutes());
+  const [child, setChild] = useState([]);
+  const refBut = useRef(null);
+  let h = new Date().getHours();
 
-  let m = new Date().getMinutes();
+  let color;
+  const onPress = (e) => {
+    let x = e.pageX - e.target.offsetLeft;
+    let y = e.pageY - e.target.offsetTop;
+    color = '#' + Math.floor(Math.random() * 0xffffff).toString(16);
+    const style = { '--crl': color, '--xb': `${x}px`, '--yb': `${y}px` };
+    let riples = <span style={style}></span>;
+
+    setChild((prev) => [...prev, riples]);
+    // setTimeout(() => {
+    //   setChild([]);
+    // }, 20000);
+  };
 
   React.useEffect(() => {
     const timer = setInterval(() => {
@@ -24,9 +40,12 @@ const EighthPage = () => {
       setMM(new Date().getMinutes());
     }, 1000);
     return () => clearInterval(timer);
-  }, [hh]);
+  }, []);
 
-  console.log(510 - (510 * hh) / 60);
+  if (hh > 12) {
+    return setHH(hh - 12);
+  }
+
   return (
     <div className="containerBox">
       <div className={`cardBox ${active ? 'active' : ''}`}>
@@ -58,7 +77,7 @@ const EighthPage = () => {
       </div>
       <div className="overflow-hidden w-[300px] h-[300px] p-3">
         <div id="time">
-          <div className="circle stroke-[#ff2972]">
+          <div className="circle stroke-[#ff2972] ">
             <div className="dots before:bg-[#ff2972]" style={{ transform: `rotateZ(${ss * 6}deg)` }}></div>
             <svg>
               <circle cx="120" cy="120" r="120" style={{ strokeDashoffset: 760 - (760 * ss) / 60 }} />
@@ -66,21 +85,35 @@ const EighthPage = () => {
           </div>
 
           <div className="circle  stroke-[#fee800]">
-            <div className="dots min_dot before:bg-[#fee800]"></div>
+            <div className="dots min_dot before:bg-[#fee800]" style={{ transform: `rotateZ(${mm * 6}deg)` }}></div>
             <svg>
-              <g rotate={-90} origin="100">
-                <circle cx="100" cy="100" r="100" style={{ strokeDashoffset: 630 - (630 * m) / 60 }} />
-              </g>
+              <circle cx="100" cy="100" r="100" style={{ strokeDashoffset: 630 - (630 * mm) / 60 }} />
             </svg>
           </div>
 
           <div className="circle stroke-[#04fc43]">
-            <div className="dots sec_dot before:bg-[#04fc43] "></div>
+            <div className="dots sec_dot before:bg-[#04fc43] " style={{ transform: `rotateZ(${hh * 30}deg)` }}></div>
             <svg>
-              <circle cx="80" cy="80" r="80" style={{ strokeDashoffset: 510 - (510 * hh) / 60 }} />
+              <circle cx="80" cy="80" r="80" style={{ strokeDashoffset: 510 - (510 * hh) / 12 }} />
             </svg>
           </div>
+          <div className="timeBx">
+            <div className=" hours text-[#ff2972]">{h < 10 ? `0` + h : h}</div>
+            <div className="minutes text-[#fee800]">{mm < 10 ? `0` + mm : mm}</div>
+            <div className="seconds text-[#04fc43]">{ss < 10 ? `0` + ss : ss}</div>
+            <div className="ap">{h > 12 ? 'PM' : 'AM'}</div>
+          </div>
         </div>
+      </div>
+      <div className="bg-[red] w-[400px] h-[300px] flex justify-center items-center gap-[20px]">
+        <a onClick={onPress} ref={refBut} href="#" className="btN">
+          {Array.isArray(child) ? child.map((el) => <>{el}</>) : []}
+          Awesome
+        </a>
+        <a href="#" className="btN">
+          {Array.isArray(child) ? child.map((el) => <>{el}</>) : []}
+          Button
+        </a>
       </div>
     </div>
   );
