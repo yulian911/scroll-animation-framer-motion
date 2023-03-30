239c124be584bce70db0bc1df9bbfbe15110a894
Yulian Melnychuk <julian95@o2.pl>
2023-03-30 08:02:36 +0200

    update

--- src/Pages/Eleven.jsx
+++ src/Pages/Eleven.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import { moon, mountains01, mountains02, road } from '../assets';
+import { MouseParallaxContainer, MouseParallaxChild } from 'react-parallax-mouse';
 import './eleven.css';
 
 const Eleven = () => {
   return (
     <div className="elevenContainer">
-      <div className="containerEleven">
-        <div id="scene">
-          <div className="layer">
-            <img src={moon} alt="img" />
-            <img src={mountains01} alt="img" />
-            <img src={mountains02} alt="img" />
-            <img src={road} alt="img" />
-          </div>
-        </div>
-      </div>
+      {/* <div className="containerEleven"> */}
+      <MouseParallaxContainer globalFactorX={0.1} globalFactorY={0.1} className="containerEleven">
+        <MouseParallaxChild factorX={0.9} factorY={3} className="layer">
+          <img src={moon} alt="img" />
+        </MouseParallaxChild>
+        <MouseParallaxChild factorX={0.2} factorY={0.2} className="layer ">
+          <img src={mountains02} alt="img" />
+        </MouseParallaxChild>
+        <MouseParallaxChild factorX={0.2} factorY={0.2} className="layer ">
+          <img src={mountains01} alt="img" />
+        </MouseParallaxChild>
+        <MouseParallaxChild factorX={0.2} factorY={-0.2} className="layer ">
+          <img src={road} alt="img" className="w-full " />
+        </MouseParallaxChild>
+      </MouseParallaxContainer>
+      {/* </div> */}
       <div className="loginEleven">
-        <h2>Sign In</h2>
+        <h2>Admin Panel</h2>
         <div className="inputBoxEleven">
           <input type={'text'} placeholder="USERNAME" />
         </div>
@@ -28,7 +35,7 @@ const Eleven = () => {
         </div>
         <div className="group">
           <a href="#">Forget Password</a>
-          <a href="#">sign up</a>
+          <a href="#">Sign up</a>
         </div>
       </div>
     </div>
